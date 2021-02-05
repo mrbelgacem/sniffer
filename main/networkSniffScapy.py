c872ca108880af0bf47dc0eece8f237d6dc520c8
mrbelgacem <mrbelgacem@outlook.com>
2021-02-05 18:28:05 +0100

    init repo

---
+++ main/networkSniffScapy.py
@@ -0,0 +1,135 @@
+
+from scapy import all
+import pygeoip
+from IPy import IP as IPLIB
+import socket
+from datetime import datetime
+import sys
+from pathlib import Path
+import logging
+
+
+def createInitLogger(logLvl=logging.INFO, consol=False):
+    # log level : DEBUG, INFO, WARNING, ERROR
+    
+    # create log directory if not exist
+    Path(pathLogDir).mkdir(parents=True, exist_ok=True)
+    
+    if consol:
+        # system consol out
+        logging.basicConfig(stream=sys.stdout, 
+                            format='%(asctime)s : %(levelname)s : %(message)s', 
+                            datefmt='%Y/%m/%d %H:%M:%S', 
+                            encoding='utf-8', 
+                            level=logLvl)##       
+    else: 
+        # log file out
+        logging.basicConfig(filename=pathLogFile, 
+                            format='%(asctime)s : %(levelname)s : %(message)s', 
+                            datefmt='%Y/%m/%d %H:%M:%S', 
+                            encoding='utf-8', 
+                            level=logLvl)##stream=sys.stdout
+
+def getInfoGeoIP(ipAddress):
+    
+    try:
+        # try to resolve the IP address
+        hostName = socket.gethostbyaddr(ipAddress)[0]
+    except:
+        # could not resolve the address
+        hostName= ""         
+    
+    # convert the IP to a valid IP object
+    ip =IPLIB(ipAddress)
+    # do not proceed if the IP is private
+    if(ip.iptype()=='PRIVATE'):
+        return 'private IP, Host Name: ' + hostName 
+        
+    try:
+        # initialize the GEOIP object
+        geoip = pygeoip.GeoIP('GeoIP.dat', flags=pygeoip.const.STANDARD)
+        # get the record info
+        ipRecord = geoip.record_by_addr(ipAddress)
+        # extract the country name
+        country = ipRecord['country_name']
+        #return the string results
+        return 'Country: %s, Host: %s'% (country,hostName)
+    except Exception:
+        return "Can't  locate " + ipAddress + " Host:" + hostName
+
+def printPacket(sourceIP,destinationIP):
+    # assemble the message need to print/save
+    return 'Source (%s): %s ---> Destination (%s): %s '% (sourceIP,getInfoGeoIP(sourceIP),destinationIP,getInfoGeoIP(destinationIP))
+  
+
+def startMonitoring(pkt):
+    listPacket = pkt.layers()
+    # log level / sys consol output
+    createInitLogger(logging.DEBUG, False)
+
+    try:      
+        if pkt.haslayer('IP'):
+            # get the source IP address
+            sourceIP = pkt.getlayer('IP').src
+            # get the destination IP address
+            destinationIP = pkt.getlayer('IP').dst
+                      
+            if(destinationIP in exclude_ips):
+                return;
+            
+            # generate a unique key to avoid duplication
+            uniqueKey = sourceIP+destinationIP
+            
+            # already processed the packet --> don't proceed further
+            if uniqueKey not in conversations:
+                # store a new key in the dict to avoid duplication
+                conversations[uniqueKey] = 1
+                # call the print packet function
+                logging.info('\r\n'+'=====Oo++oO=====Oo++oO=====')
+                logging.info(printPacket(sourceIP, destinationIP))
+                for subPkt in listPacket:
+                    logging.debug(subPkt)
+            else:
+                conversations[uniqueKey] = +1
+        
+    except Exception as ex:
+        logging.error("Exception ex:" + str(ex))
+        pass
+
+def main():
+    # start sniffing by filtering only the IP packets without storing anything inside the memory.
+    all.sniff(prn=startMonitoring,store=0,filter="ip")
+    
+
+if __name__ == '__main__':
+    # packet processed
+    conversations={}
+    # Exclude local loop, .... etc '10.0.2.133'
+    exclude_ips= ['127.0.0.1']
+    # log properties
+    dateTime = datetime.today().strftime('%Y%m%d %H:%M:%S')
+    pathLogDir = './log/network_monitor_log/'
+    pathLogFile = pathLogDir+dateTime + '.log'
+    
+    main()
+    
+
+##[+] Sub Packet (<class 'scapy.layers.l2.Ether'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.IP'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.UDP'>)
+##[+] Sub Packet (<class 'scapy.layers.dns.DNS'>)
+    
+##[+] Sub Packet (<class 'scapy.layers.l2.Ether'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.IP'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.TCP'>)
+##[+] Sub Packet (<class 'scapy.packet.Raw'>)
+    
+##[+] Sub Packet (<class 'scapy.layers.l2.Ether'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.IP'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.TCP'>)
+    
+##[+] Sub Packet (<class 'scapy.layers.l2.Ether'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.IP'>)
+##[+] Sub Packet (<class 'scapy.layers.inet.UDP'>)
+##[+] Sub Packet (<class 'scapy.layers.ntp.NTPHeader'>)
+
